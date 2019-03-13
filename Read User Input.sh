7ebf60696bbfbabab14d55894b566441a4266f50
Aleksandar Jovanovic <sasapezos1995@gmail.com>
2019-03-13 16:20:51 +0100

    Update Read User Input.sh

--- Read User Input.sh
+++ Read User Input.sh
@@ -1,25 +1,26 @@
 #! /bin/bash
 
-#echo "Enter name : "
-#read name
-#echo "Entered name : $name"
+echo "Enter name : "
+read name
+echo "Entered name : $name"
+#Asks for an input and prints the input
 
-
-#read -p 'username : ' user_var
+read -p 'username : ' user_var
 #Allows to enter username on the same line.
 
-#read -sp 'password : ' userPass_var
+read -sp 'password : ' userPass_var
 #Silences the input so it is not shown on terminal.
 
-#echo
-#echo "username : $user_var"
-#echo "password : $userPass_var"
+echo
+echo "username : $user_var"
+echo "password : $userPass_var"
+#Prints the previously inputed read arguments.
 
 
-#echo "Enter names : "
-#read -a names
-#Puts input into an array
-#echo "Names : ${names[0]}, ${names[1]}"
+echo "Enter names : "
+read -a names
+#Puts input names into an array
+echo "Names : ${names[0]}, ${names[1]}"
 
 
 echo "Enter names : "
