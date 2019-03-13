c5dfa4bb61d5968e06ae6b26137f02870b917518
Aleksandar Jovanovic <sasapezos1995@gmail.com>
2019-03-13 16:23:44 +0100

    Update Introductions, System and User variables.sh

--- Introductions, System and User variables.sh
+++ Introductions, System and User variables.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
-# this is a comment
-echo "Hello World" # this is also a comment
+# This is a comment
+echo "Hello World" # This is also a comment
 
 echo Our shell name is $BASH
 echo Our shell version is $BASH_VERSION
 echo Our home directory is $HOME
 echo Our Present Working Directory is $PWD
+#Self explanatory
 
 name=Alex
+#Declaring variable name as string Alex
 val=10
+#Declaring variable val as 10
 echo The name is $name
 echo value $val
